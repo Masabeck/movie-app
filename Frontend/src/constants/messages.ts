b999db2b91fab64e9dc5f0be24a6fb1ae9ff3d8e
Masabeck <masabeck13@gmail.com>
2025-07-18 11:24:53 +0500

    Centralize error messages in frontend using constants file

---
+++ Frontend/src/constants/messages.ts
@@ -0,0 +1,11 @@
+// src/constants/messages.ts
+
+const MESSAGES = {
+  FILL_ALL_FIELDS: "Please fill in all fields.",
+  INVALID_EMAIL: "Please enter a valid email address.",
+  SHORT_PASSWORD: "Password must be at least 6 characters.",
+  LOGIN_FAILED: "Login failed",
+  SIGNUP_FAILED: "Signup failed"
+};
+
+export default MESSAGES;
